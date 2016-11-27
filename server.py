7212c617715c293455d5fec2cfeeac1e21f065ae
paulrevere4 <revereiv@gmail.com>
2016-11-27 17:40:30 -0500

    Move server to class

--- server.py
+++ server.py
@@ -14,13 +14,48 @@ from client_listener import *
 from serializer import Serializer
 
 # ==============================================================================
-# Checks the task_queue for tasks and completes them
 #
-def run(task_queue):
-    while True:
-        if not task_queue.empty():
-            task = task_queue.get()
-            print task
+class Server(object):
+    """
+    Object that will carry out the responsibilities of the server
+    """
+    # ==========================================================================
+    # Setup member variables
+    #
+    def __init__(self, server_num, host, cli_listen_port):
+        self.server_num = server_num
+
+        self.host = host
+        self.cli_listen_port = cli_listen_port
+
+        # make the task queue
+        self.task_queue = Queue.Queue()
+
+    # ==========================================================================
+    # Starts the worker threads
+    #
+    def start_threads(self):
+        # make the threads
+        client_listener_thread = Thread( \
+            target=run_client_listener, \
+            args=(self.task_queue, self.host, self.cli_listen_port))
+
+        # set threads as daemons so we can kill them
+        client_listener_thread.daemon = True
+
+        # start threads
+        client_listener_thread.start()
+
+
+    # ==========================================================================
+    # Checks the task_queue for tasks and completes them. Main worker thread of
+    # the Server.
+    #
+    def run(self):
+        while True:
+            if not self.task_queue.empty():
+                task = self.task_queue.get()
+                print "MAIN_WORKER: COMPLETING TASK:", task
 
 # ==============================================================================
 #
@@ -36,17 +71,7 @@ if __name__ == "__main__":
     host = str(sys.argv[1])
     cli_listen_port = int(sys.argv[2])
 
-    # make the task queue
-    task_queue = Queue.Queue()
-
-    # make the threads
-    client_listener_thread = Thread(target=run_client_listener, args=(task_queue, host, cli_listen_port))
-
-    # set threads as daemons so we can kill them
-    client_listener_thread.daemon = True
-
-    # start threads
-    client_listener_thread.start()
-
-    # run main
-    run(task_queue)
+    # setup server object and run
+    s = Server(0, host, cli_listen_port)
+    s.start_threads()
+    s.run()
