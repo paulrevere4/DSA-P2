253bdf823c58dc97ccef53c9a64219110378cb7e
paulrevere4 <revereiv@gmail.com>
2016-11-27 23:00:03 -0500

    Switch back to old client and listener

--- client_listener.py
+++ client_listener.py
@@ -24,11 +24,10 @@ def run_client_listener(task_queue, host, port):
     while True:
         time.sleep(.1)
         c, addr = s.accept()    # Establish connection with client.
-        while True:
-            print "CLIENT_LISTENER: GOT CONNECTION FROM", addr
-            received = Serializer.deserialize(c.recv(1024))
-            task = ["CLIENT", received]
-            print "CLIENT_LISTENER: %s" %task
-            task_queue.put((5,task)) # TODO solidify task priority
-            # c.close()                # Close the connection
-            # print "CLIENT_LISTENER: CONNECTION CLOSED"
+        print "CLIENT_LISTENER: GOT CONNECTION FROM", addr
+        received = Serializer.deserialize(c.recv(1024))
+        task = ["CLIENT", received]
+        print "CLIENT_LISTENER: %s" %task
+        task_queue.put((5,task)) # TODO solidify task priority
+        c.close()                # Close the connection
+        print "CLIENT_LISTENER: CONNECTION CLOSED"
